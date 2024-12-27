22a82764a8f4c24b3d95858882dfbea0257ae84f
abnerugeda <abner.ugeda@carefy.com.br>
2024-12-27 10:53:10 -0300

    feat: conectando com o banco de dados

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	http.HandleFunc("/", index)
+	_, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go")
+	if err != nil {
+		panic(err)
+	}
 
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul bonita", Preco: 1000, Quantidade: 10},
+		{"Tenis", "confortavel", 100, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
