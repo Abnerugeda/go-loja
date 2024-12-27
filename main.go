c7101356643410e8ab20e968c29eab8295154b65
abnerugeda <abner.ugeda@carefy.com.br>
2024-12-27 11:41:30 -0300

    feat: mostrando na listagem os dados reais do banco de dados

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var (
+	db *sql.DB
+)
+
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -18,7 +24,9 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	_, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/go")
+	var err error
+	db, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/go")
+
 	if err != nil {
 		panic(err)
 	}
@@ -28,10 +36,34 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul bonita", Preco: 1000, Quantidade: 10},
-		{"Tenis", "confortavel", 100, 1},
+	allProducts, err := db.Query("SELECT * FROM produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for allProducts.Next() {
+
+		err = allProducts.Scan(&p.Id, &p.Nome, &p.Descricao, &p.Preco, &p.Quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtos = append(produtos, p)
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+}
+
+func insertProduct(produto Produto) sql.Result {
+	var res sql.Result
+
+	res, _ = db.Exec(fmt.Sprintf(`INSERT INTO produtos (nome,descricao,preco,quantidade) VALUES
+		('%s','%s',%f,%d)`, produto.Nome, produto.Descricao, produto.Preco, produto.Quantidade))
+
+	return res
 }
